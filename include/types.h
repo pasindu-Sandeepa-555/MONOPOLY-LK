3c35147dc2e95b96afe340b9beba7a6612587e49
Pasindu-sandeepa-555 <pasindusandeepa549@gmail.com>
2026-08-13 00:37:34 +0530

    Define core game enums

---
+++ include/types.h
@@ -0,0 +1,33 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+enum strategy {
+	AGGRESSIVE,
+	CONSERVATIVE,
+	RISK_TAKER,
+	TRADER
+
+};
+
+enum playerstatus {
+	SOLVENT,
+	BANKCRUPT
+
+};
+
+
+enum spacetypes {
+	GO,
+	PROPERTY,
+	RAILWAY,
+	UTILITY,
+	TAX,
+	EVENT,
+	JAIL,
+	GO_TO_JAILL,
+	FREE_PARKING
+
+};
+
+#endif
+
